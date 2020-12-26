cee8c1260368897dbb5fe264ae2cc6bfee00f752
Jeffrey Rose <jtr219@outlook.com>
2020-12-26 13:30:12 -0500

    set up index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1>Full-Stack React 👋</h1>
     <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+      Let's practice connecting React to a backend! In this simple project, we'll build the following:
     </p>
+    <ol>
+      {[
+        "Set up react client with async call to server",
+        "Set up server with Mock DB / API response to client",
+        "Add CRUD calls to server from client",
+        "Refactor Mock DB to use Mongo/ Mongoose",
+        "Integrate GraphQL/ Apollo",
+        "Set up user login/ auth",
+        "Manage Cookies/ session ID"
+      ].map((task, i) => (<li key={`task-${i}`}>{task}</li>))}
+    </ol>
+
   </Layout>
 )
 
